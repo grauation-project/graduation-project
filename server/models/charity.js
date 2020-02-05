620d17206dd55edef60f6fd6416c59e6a6cb6906
Mona Soliman <47220624+MonaSoliman009@users.noreply.github.com>
2020-02-05 22:56:03 +0200

    posts schema+ some validation and auth for api

--- server/models/charity.js
+++ server/models/charity.js
@@ -2,65 +2,56 @@ const mongoose = require("mongoose")
 
 const Schema = mongoose.Schema
 const charityschema = new Schema({
-    name:
-    {
-        type:String,
-        required:true,
-        max:25
+  name: {
+    type: String,
+    required: true,
+    max: 25
 
-    },
-    img:
-    {
-        type:String
-    },
-    email:
-    {
-        type:String,
-        required:true,
-        unique:true,
-        max:225
+  },
+  img: {
+    type: String
+  },
+  email: {
+    type: String,
+    required: true,
+    unique: true,
+    max: 225
 
-    },
-    password:
-    {
-        type:String,
-        required:true,
-        min:8,
-        max:16
-    },
-    phone:
-    {
-        type:Number,
-        required:true,
-        max:10
-       
-    },
-    bankaccount:
-    {
-        type:Number,
-        unique:true
+  },
+  password: {
+    type: String,
+    required: true,
+    min: 8,
+    max: 16
+  },
+  phone: {
+    type: Number,
+    required: true,
+    max: 10
 
-    },
-    address:
-      {country:
-         {
-           type:String,
-           required:true,
-         },
-    
-        city:
-         {
-           type:String,
-           required:true  
-         },
-         street:
-         {
-             type:String,
-             required:true
-         }
+  },
+  bankaccount: {
+    type: Number,
+    unique: true
 
+  },
+  address: {
+    country: {
+      type: String,
+      required: true,
+    },
+
+    city: {
+      type: String,
+      required: true
+    },
+    street: {
+      type: String,
+      required: true
     }
+
+  }
 
 })
 
-module.exports = mongoose.model("charity", charityschema)
+module.exports = mongoose.model("charity", charity)
