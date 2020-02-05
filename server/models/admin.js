cc964fe5f70d68330ab137214273fa6670006adc
Madona Poles <madona.poles@gmail.com>
2020-02-05 16:44:29 +0200

    Create admin.js
    admin-models

---
+++ server/models/admin.js
@@ -0,0 +1,20 @@
+var mongoose = require('mongoose');
+ 
+var Schema = mongoose.Schema;
+
+var admin = new Schema({
+  email:{
+      type:String,
+      required:true,
+      unique:true
+  },
+  password:{
+    type:Number,
+    required:true,
+    unique:true,
+    minlength:6,
+    maxlength:15
+  }
+});
+
+mongoose.model("admin",admin);
