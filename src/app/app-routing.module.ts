31b4de0ec2f4c072baec819c27ddeecb459937f4
Madona Poles <madona.poles@gmail.com>
2020-02-10 09:43:35 +0200

    footer

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,13 +4,15 @@ import { HomeregisterationComponent } from "./homeregisteration/homeregisteratio
 import { AboutusComponent } from "./aboutus/aboutus.component";
 import { HowitworkComponent } from "./howitwork/howitwork.component";
 import { ContactComponent } from "./contact/contact.component";
+import { CharityAccountComponent } from './charity-account/charity-account/charity-account.component';
 
 const routes: Routes = [
   { path: "", component: HomeregisterationComponent },
 
   { path: "home", component: HomeregisterationComponent },
   { path: "about", component: AboutusComponent },
-  { path: "contact", component: ContactComponent }
+  { path: "contact", component: ContactComponent },
+  { path: "cacharityaccount",component:CharityAccountComponent }
 ];
 
 @NgModule({
